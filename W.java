4f6b73f57c021477ae5df17a24e7ede8cc41614a
panlij <397166632@qq.com>
2016-05-04 09:53:13 +0800

    创建w.java

---
+++ W.java
@@ -0,0 +1,11 @@
+public class w
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    https://github.com/panlij/hello-world-panlij.git
+}
